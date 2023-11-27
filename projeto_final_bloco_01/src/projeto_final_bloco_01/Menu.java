5b0e645871637e5bf88a4dbceea98d4fcbf7d0ad
unknown <gercidio.valeriano@gmail.com>
2023-11-27 14:13:24 -0300

    menu/branch01

---
+++ projeto_final_bloco_01/src/projeto_final_bloco_01/Menu.java
@@ -0,0 +1,43 @@
+package projeto_final_bloco_01;
+
+import java.util.Scanner;
+
+import ecommerce.util.Cores;
+
+public class Menu {
+
+	public static void main(String[] args) {
+Scanner leia = new Scanner(System.in);
+		
+		int id, tipo, opcao;
+		String nome;
+		float preco;
+		
+while(true) {
+	
+			System.out.println(Cores.TEXT_WHITE_BOLD_BRIGHT + Cores.ANSI_BLACK_BACKGROUND
+					+ "*****************************************************");
+			System.out.println("                                                     ");
+			System.out.println("                ABC ESPORTES	                     ");
+			System.out.println("                                                     ");
+			System.out.println("*****************************************************");
+			System.out.println("                                                     ");
+			System.out.println("            1 - Criar Produto                        ");
+			System.out.println("            2 - Listar todos os Produtos             ");
+			System.out.println("            3 - Consultar Produtos por ID            ");
+			System.out.println("            4 - Atualizar Produtos		             ");
+			System.out.println("            5 - Apagar Produto                       ");
+			System.out.println("                                                     ");
+			System.out.println("*****************************************************");
+			System.out.println("Entre com a opção desejada:                          ");
+			System.out.println("                                                     " 
+			+ Cores.TEXT_RESET);
+			
+		 opcao = leia.nextInt();
+		
+}
+	}
+
+	}
+
+
